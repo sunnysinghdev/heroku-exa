17a662f405d8eff22a1700e9758531d126ad7145
Sunny Singh <sunnysingh.vpcoe@gmail.com>
2021-05-26 15:46:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World'+process.env.DATABASE_URL);
 });
 
 server.listen(port, hostname, () => {
